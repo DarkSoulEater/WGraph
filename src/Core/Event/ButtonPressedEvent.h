3ef1566a62347f4aaf2969e109bbd4329434f491
Platonov Timofey <platonov.ta@phystech.edu>
2023-10-30 17:38:59 +0300

    * Add index for buttons set

--- src/Core/Event/ButtonPressedEvent.h
+++ src/Core/Event/ButtonPressedEvent.h
@@ -6,11 +6,12 @@
 
 class ButtonPressedEvent : public Event {
 public:
-    ButtonPressedEvent(PushButton* button) : Button(button) {}
+    ButtonPressedEvent(PushButton* button, int index = -1) : Button(button), Index(index) {}
 
     virtual EventType Type() const { return EventType::ButtonPressed; }    
 public:
     PushButton* Button = nullptr;
+    int Index = -1;
 };
 
 #endif // ___BUTTONPRESSEDEVENT_H___
