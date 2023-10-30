d25414ff8c1c4a5998606a8136515c918907b17d
Platonov Timofey <platonov.ta@phystech.edu>
2023-10-30 17:29:43 +0300

    Remove key reset tool

--- src/Photoshop/UI/Palette.cpp
+++ src/Photoshop/UI/Palette.cpp
@@ -4,33 +4,60 @@
 #include "Photoshop/Tool/CircleTool.h"
 #include "Photoshop/Event/ToolChangedEvent.h"
 
+#include "Core/Action/AcDebug.h"
+#include "Core/Action/ActSendEvent.h"
+
+#include "Print.h"
+
+Palette::Palette(Window &main_window) : main_window_(main_window) {
+    SetPosition({10, 50});
+    SetBoundsSize({144, 600}, false);
+
+    tool_button_cnt = palette_.GetToolsCount();
+
+    for (size_t i = 0; i < tool_button_cnt; ++i) {
+        Vector2 button_size(66, 52);
+        tool_buttons.push_back(new PushButton(button_size));
+        tool_buttons[i]->SetParent(this);
+        tool_buttons[i]->SetPosition(Vector2(6  + button_size.X * (i % 2), 
+                                             10 + button_size.Y * (i / 2)));
+        tool_buttons[i]->SetAction(new ActSendEvent(new ButtonPressedEvent(tool_buttons[i], i), this));
+    }
+
+    tool_buttons[0]->UpdateTexture("assets/Tool/PenTool.png");
+    tool_buttons[1]->UpdateTexture("assets/Tool/BrushTool.png");
+    tool_buttons[2]->UpdateTexture("assets/Tool/CircleTool.png");
+
+    tool_buttons[tool_active_button]->SetColor(tool_active_button_color);
+}
+
 void Palette::OnEvent(const Event *event) {
     if (event->Type() == EventType::Key) {
         OnKeyEvent(*static_cast<const KeyEvent*>(event));
+    } else if (event->Type() == EventType::ButtonPressed) {
+        OnButtonPressedEvent(*static_cast<const ButtonPressedEvent*>(event));
+        return;
     }
+
+    Widget::OnEvent(event);
+}
+
+void Palette::OnMouseEvent(const MouseEvent &event) {
+}
+
+void Palette::OnDrawEvent(const DrawEvent &event) {
+    sf::RectangleShape panel;
+    panel.setFillColor(Color(79, 79, 79));
+    panel.setOutlineColor(Color(35, 35, 35));
+    panel.setOutlineThickness(-2);
+    panel.setPosition(MapToGlobal());
+    panel.setSize(Vector2(144, 600));
+    event.Window.draw(panel);
+
+    // Widget::OnDrawEvent(event);
 }
 
 void Palette::OnKeyEvent(const KeyEvent &event) {
-    // Change Tool
-    Tool* tool = nullptr;
-    if (event.Keys[KeyCode::P].Pressed) {
-        std::cout << "LOG[Palette]: Set Pen Tool\n";
-        tool = new PenTool(palette_);
-    } else if (event.Keys[KeyCode::B].Pressed) {
-        std::cout << "LOG[Palette]: Set Brash Tool\n";
-        tool = new BrushTool(palette_);
-    } else if (event.Keys[KeyCode::C].Pressed) {
-        std::cout << "LOG[Palette]: Set Circle Tool\n";
-        tool = new CircleTool(palette_);
-    }
-
-    if (tool) {
-        ToolChangedEvent event;
-        main_window_.OnEvent(&event);
-
-        palette_.ResetTool(tool);
-    }
-
     // Change Color
     if (event.Keys[KeyCode::W].Pressed) {
         std::cout << "LOG[Palette]: Set White foreground color\n";
@@ -41,5 +68,37 @@ void Palette::OnKeyEvent(const KeyEvent &event) {
     } else if (event.Keys[KeyCode::M].Pressed) {
         std::cout << "LOG[Palette]: Set Magneta foreground color\n";
         palette_.SetForegroundColor(Color::Magenta);
+    }
+}
+
+void Palette::OnButtonPressedEvent(const ButtonPressedEvent &event) {
+    // Log("Pressed: %d", event.Index);
+    
+    if (palette_.IsNowTool(event.Index)) {
+        ToolChangedEvent tool_event;
+        main_window_.OnEvent(&tool_event);
+        palette_.ResetTool(event.Index);
+
+        tool_buttons[tool_active_button]->SetColor(tool_deactive_button_color);
+        tool_buttons[event.Index]->SetColor(tool_active_button_color);
+        tool_active_button = event.Index;
+
+        switch (event.Index) {
+            case 0: {
+                Log("[Pallete] Switch to Pen Tool");
+            } break;
+
+            case 1: {
+                Log("[Pallete] Switch to Brush Tool");
+            } break;
+
+            case 2: {
+                Log("[Pallete] Switch to Circle Tool");
+            } break;
+
+            default: {
+                Log("[Pallete] Switch to Unknow Tool");
+            } break;
+        }
     }
 }
--- src/Photoshop/UI/Palette.h
+++ src/Photoshop/UI/Palette.h
@@ -2,19 +2,32 @@
 #define ___PALETTE_H___
 
 #include "Core/Window.h"
+#include "Core/Event/KeyEvent.h"
 #include "UI/Widget.h"
 #include "Photoshop/Tool/ToolPalette.h"
-#include "Core/Event/KeyEvent.h"
+#include "UI/PushButton.h"
+#include "Core/Event/ButtonPressedEvent.h"
+#include "Photoshop/Tool/Color.h"
 
 class Palette : public Widget {
 public:
-    Palette(Window& main_window) : main_window_(main_window) {}
+    Palette(Window& main_window);
 
     ToolPalette& GetToolPalette() { return palette_; }
 
     virtual void OnEvent(const Event* event) override;
-
+    virtual void OnMouseEvent(const MouseEvent& event) override;
+    virtual void OnDrawEvent(const DrawEvent& event) override;
     void OnKeyEvent(const KeyEvent& event);
+    void OnButtonPressedEvent(const ButtonPressedEvent& event);
+
+private:
+    size_t tool_button_cnt = -1;
+    size_t tool_active_button = 0;
+    std::vector<PushButton*> tool_buttons;
+
+    Color tool_active_button_color = Color(59, 59, 59);
+    Color tool_deactive_button_color = Color(0);
 
 private:
     Window& main_window_;
