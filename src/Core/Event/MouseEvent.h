7d6265e2b2ec0b0caeccd0855fbb1a95039447df
Platonov Timofey <platonov.ta@phystech.edu>
2023-10-30 17:39:40 +0300

    * Add Dump() for mouse event

--- src/Core/Event/MouseEvent.h
+++ src/Core/Event/MouseEvent.h
@@ -16,7 +16,6 @@ public:
     virtual ~MouseEvent() {};
 
     virtual EventType Type() const override { return EventType::Mouse; }
-
 public:
     static const uint32_t kMouseButtonCount = 3;
 
@@ -36,6 +35,22 @@ public:
     Key Shift;
     Key Ctrl;
     Key Alt;
+
+public:
+    virtual void Dump() const override {
+        std::cerr << "[" << ToString(Type()) << "] {\n"; 
+        std::cerr << "\tMouse old position: {" << MouseOldPosition.x << ", " << MouseOldPosition.y << "}\n";
+        std::cerr << "\tMouse position:     {" << MousePosition.x << ", " << MousePosition.y << "}\n";
+        std::cerr << "\tMouse delta:        {" << DeltaMouse.x << ", " << DeltaMouse.y << "}\n";
+        for (int ind = 0; ind < kMouseButtonCount; ++ind) {
+            std::cerr << "\tMouse button " << ind << ": {";
+            if (MouseButton[ind].Pressed) std::cerr << "pressed";
+            if (MouseButton[ind].Pressed && MouseButton[ind].Released) std::cerr << ", ";
+            if (MouseButton[ind].Released) std::cerr << "released";
+            std::cerr << "}\n";
+        }
+        std::cerr << "}\n";
+    }
 };
 
 #endif // ___MOUSEEVENT_H___
